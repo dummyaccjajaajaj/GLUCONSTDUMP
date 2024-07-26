97af34ac36ff061ffae86ba6870e93d93cf54013
zack <136998193+dummyaccjajaajaj@users.noreply.github.com>
2024-07-26 14:41:50 +0800

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -21,13 +21,13 @@ try {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // SMTP server address
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'SMTP_USERNAME'; // SMTP username
-        $mail->Password   = 'SMTP_PASSWORD'; // SMTP password
+        $mail->Username   = getenv('SMTP_USERNAME'); // SMTP username
+        $mail->Password   = getenv('SMTP_PASSWORD'); // SMTP password
         $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
         $mail->Port       = 587; // TCP port to connect to
 
     // Recipients
-    $mail->setFrom('SMTP_USERNAME', '');
+    $mail->setFrom(getenv('SMTP_USERNAME'), '');
     $mail->addAddress('kenkenu758@gmail.com', ''); // Add a recipient
 
     // Content
